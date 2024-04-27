b9367ea6d47a0909d0ff93c3df2545191a5b6477
RVSHANDILYA <shandilyapuram544@gmail.com>
2024-04-27 20:48:22 +0530

    Ask 3 numbers from User and Calculate the Average.

---
+++ Q3.py
@@ -0,0 +1,8 @@
+'''
+Q3. Ask 3 numbers from User and Calculate the Average.
+'''
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=int(input("Enter 3rd number:"))
+res=(a+b+c)/3
+print("The Average of 3 number's is",res)
