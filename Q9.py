3dddf49ff3bf27d3679bda82180f44cc3a5f8e1a
RVSHANDILYA <shandilyapuram544@gmail.com>
2024-04-28 10:11:54 +0530

    Square or Rectangle?

---
+++ Q9.py
@@ -0,0 +1,10 @@
+'''
+Q9.Take values of length and breadth of a rectangle from user and check if
+it is square or not.
+'''
+l=float(input("Enter Length:"))
+b=float(input("Enter breadth:"))
+if l==b:
+    print("This is a Square")
+else:
+  print("This is a Rectangle")   
