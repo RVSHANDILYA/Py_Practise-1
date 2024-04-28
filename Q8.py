e7f2d9415aa17f0589c70858f1597cbcbd91af37
RVSHANDILYA <shandilyapuram544@gmail.com>
2024-04-28 10:09:12 +0530

    Number is ODD or EVEN?

---
+++ Q8.py
@@ -0,0 +1,8 @@
+'''
+Q8.Ask a number from user. Print if the number is ODD or EVEN
+'''
+num=int(input("Enter a number:"))
+if num%2==0:
+    print(f"The number {num} is Even")
+else:
+    print(f"The number {num} is Odd")
