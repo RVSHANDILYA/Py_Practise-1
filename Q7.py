04841f1a3f9b49478d3c73b12a2e73ca8a59ce03
RVSHANDILYA <shandilyapuram544@gmail.com>
2024-04-28 10:07:06 +0530

    Check if the number entered by User is divisible by 3 or not

---
+++ Q7.py
@@ -0,0 +1,8 @@
+'''
+Q7.Check if the number entered by User is divisible by 3 or not.
+'''
+num=int(input("Enter a Number:"))
+if num%3==0:
+    print(f"The entered number {num} is divisible by 3")
+else:
+    print(f"The entered number {num} is not divisible by 3")
